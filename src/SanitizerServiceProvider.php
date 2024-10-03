2352502e6fed1885ed07c0c9572ac3c2e30cbf63
Fhauze <49869169+fhauze@users.noreply.github.com>
2024-10-03 16:51:49 +0700

    Update SanitizerServiceProvider.php

--- src/SanitizerServiceProvider.php
+++ src/SanitizerServiceProvider.php
@@ -4,16 +4,29 @@ namespace Fir2be\Sanitizer;
 
 use Illuminate\Support\ServiceProvider;
 
-class SecurityDetectorServiceProvider extends ServiceProvider
+class SanitizerServiceProvider extends ServiceProvider
 {
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
     public function boot()
     {
-        // Register middleware
-        $this->app['router']->pushMiddlewareToGroup('web', \YourVendor\SecurityDetector\Http\Middleware\DetectInjectionMiddleware::class);
+        // Publikasi file middleware jika diperlukan, contoh:
+        $this->publishes([
+            __DIR__ . '/Http/Middleware/SanitizerMiddleware.php' => base_path('app/Http/Middleware/SanitizerMiddleware.php'),
+        ], 'sanitizer-middleware');
     }
 
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
     public function register()
     {
-        // Register any bindings if necessary
+        // Register Middleware secara manual jika diperlukan
+        $this->app['router']->aliasMiddleware('sanitizer', \Fir2be\Sanitizer\Http\Middleware\SanitizerMiddleware::class);
     }
 }
